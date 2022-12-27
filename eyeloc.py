3f330e6c477364a6d46c84f65ae4d60228dbfb06
Mohamed Kamal <medoking91@gmail.com>
2022-12-27 14:51:42 +0200

    added train set

--- eyeloc.py
+++ eyeloc.py
@@ -1,4 +1,5 @@
 import io
+import math
 
 import cv2
 import dlib
@@ -14,45 +15,59 @@ class eyelocator:
             cv2.data.haarcascades + 'haarcascade_frontalface_default.xml'
         )
         self.detector = dlib.get_frontal_face_detector()
-        self.predictor = dlib.shape_predictor("./shape_predictor_68_face_landmarks.dat")
+        self.predictor = dlib.shape_predictor(
+            "./shape_predictor_68_face_landmarks.dat")
 
     def train(self, images):
+        taken, skipped = 0, 0
         for img in images:
-            img_grey = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-            faces = self.face_cascade.detectMultiScale(img_grey, 1.3, 5)
-            # TODO: rotate and scale randomly
-            (y, x, h, w) = faces[0]
-            resize = cv2.resize(img_grey[x : x + w, y : y + h], (128, 128))
-            detect = self.detector(resize, 1)
-            shape = self.predictor(resize, detect[0])
-            # left eye location
-            x1, y1 = (shape.part(37).x + shape.part(40).x) // 2, (
-                shape.part(37).y + shape.part(40).y
-            ) // 2
-            # right eye location
-            x2, y2 = (shape.part(43).x + shape.part(46).x) // 2, (
-                shape.part(43).y + shape.part(46).y
-            ) // 2
-            left_gauss = self.gauss(x1, y1)
-            right_gauss = self.gauss(x2, y2)
+            try:
+                img_grey = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+                faces = self.face_cascade.detectMultiScale(img_grey, 1.3, 5)
+                # TODO: rotate and scale randomly
+                (y, x, h, w) = faces[0]
+                resize = cv2.resize(img_grey[x: x + w, y: y + h], (128, 128))
+                detect = self.detector(resize, 1)
+                shape = self.predictor(resize, detect[0])
+                # left eye location
+                x1, y1 = (shape.part(37).x + shape.part(40).x) // 2, (
+                    shape.part(37).y + shape.part(40).y
+                ) // 2
+                # right eye location
+                x2, y2 = (shape.part(43).x + shape.part(46).x) // 2, (
+                    shape.part(43).y + shape.part(46).y
+                ) // 2
+                left_gauss = self.gauss(x1, y1)
+                right_gauss = self.gauss(x2, y2)
 
-            left_gauss_f = fft.fft2(left_gauss)
-            right_gauss_f = fft.fft2(right_gauss)
-            img_f = fft.fft2(resize)
-
-            self.left_eye_asef += left_gauss_f / img_f
-            self.right_eye_asef += right_gauss_f / img_f
-
+                left_gauss_f = fft.fft2(left_gauss)
+                right_gauss_f = fft.fft2(right_gauss)
+                img_f = fft.fft2(resize)
+                # if math.isinf(np.abs(f1)) or math.isinf(np.abs(f2)):
+                #     skipped += 1
+                #     continue
+                f1 = np.divide(left_gauss_f, img_f,
+                               out=np.zeros_like(img_f), where=img_f != 0)
+                f2 = np.divide(right_gauss_f, img_f,
+                               out=np.zeros_like(img_f), where=img_f != 0)
+                self.left_eye_asef += f1
+                self.right_eye_asef += f2
+                taken += 1
+            except:
+                skipped += 1
+                continue
         n = len(images)
         self.left_eye_asef / n
         self.right_eye_asef / n
+        print(f'Finished training, taken = {taken}, skipped = {skipped}')
 
     def gauss(self, x, y, sigma=4.0):
         gauss = np.zeros([128, 128])
         for i in range(127):
             for j in range(127):
                 gauss[i, j] = np.exp(
-                    -1.0 * (np.square(i - x) + np.square(j - y)) / np.square(sigma)
+                    -1.0 * (np.square(i - x) + np.square(j - y)) /
+                    np.square(sigma)
                 )
 
         return gauss
