a167d4e35bdc35e33a82480e19d06d7faaaebd19
Mohamed Kamal <medoking91@gmail.com>
2022-12-28 20:11:29 +0200

    feat: nose and mouth detection

--- EyeLocator/eyeloc.py
+++ EyeLocator/eyeloc.py
@@ -14,16 +14,21 @@ class eyelocator:
         else:
             self.left_eye_asef = fft.fft2(np.zeros([128, 128]))
             self.right_eye_asef = fft.fft2(np.zeros([128, 128]))
+            self.nose_asef = fft.fft2(np.zeros([128, 128]))
+            self.mouth_asef = fft.fft2(np.zeros([128, 128]))
 
         self.face_cascade = cv2.CascadeClassifier(
             cv2.data.haarcascades + 'haarcascade_frontalface_default.xml'
         )
         self.detector = dlib.get_frontal_face_detector()
-        self.predictor = dlib.shape_predictor("./shape_predictor_68_face_landmarks.dat")
+        self.predictor = dlib.shape_predictor(
+            "./shape_predictor_68_face_landmarks.dat")
 
     def load(self, path="./"):
         self.left_eye_asef = np.load(path + "/left_eye_asef.npy")
         self.right_eye_asef = np.load(path + "/right_eye_asef.npy")
+        self.nose_asef = np.load(path + "/nose_asef.npy")
+        self.mouth_asef = np.load(path + "/mouth_asef.npy")
 
     def train(self, images):
         taken, skipped = 0, 0
@@ -39,7 +44,7 @@ class eyelocator:
                 # for _ in range(8):
                 # rx, ry = np.random.randint(-4, 4, (2))
                 (y, x, h, w) = faces[0]
-                resize = cv2.resize(img_grey[x : x + w, y : y + h], (128, 128))
+                resize = cv2.resize(img_grey[x: x + w, y: y + h], (128, 128))
                 # angle = ((np.random.rand() * np.pi) / 8) - (np.pi / 16)
                 # resize = imutils.rotate(resize, angle)
                 detect = self.detector(resize, 1)
@@ -52,29 +57,51 @@ class eyelocator:
                 x2, y2 = (shape.part(43).x + shape.part(46).x) // 2, (
                     shape.part(43).y + shape.part(46).y
                 ) // 2
-                left_gauss = self.gauss(x1, y1)
-                right_gauss = self.gauss(x2, y2)
-
-                left_gauss_f = fft.fft2(left_gauss)
-                right_gauss_f = fft.fft2(right_gauss)
+                # nose tip
+                x3, y3 = shape.part(30).x, shape.part(30).y
+                # mouth center
+                x4, y4 = (shape.part(54).x + shape.part(48).x) // 2, (
+                    shape.part(54).y + shape.part(48).y
+                ) // 2
+                # Applying filters
+                left_eye_gauss = self.gauss(x1, y1)
+                right_eye_gauss = self.gauss(x2, y2)
+                nose_gauss = self.gauss(x3, y3)
+                mouth_gauss = self.gauss(x4, y4)
+                # fourier transform
+                left_eye_gauss_f = fft.fft2(left_eye_gauss)
+                right_eye_gauss_f = fft.fft2(right_eye_gauss)
+                nose_gauss_f = fft.fft2(nose_gauss)
+                mouth_gauss_f = fft.fft2(mouth_gauss)
                 img_f = fft.fft2(resize)
                 # if math.isinf(np.abs(f1)) or math.isinf(np.abs(f2)):
                 #     skipped += 1
                 #     continue
                 f1 = np.divide(
-                    left_gauss_f, img_f, out=np.zeros_like(img_f), where=img_f != 0
+                    left_eye_gauss_f, img_f, out=np.zeros_like(img_f), where=img_f != 0
                 )
                 f2 = np.divide(
-                    right_gauss_f, img_f, out=np.zeros_like(img_f), where=img_f != 0
+                    right_eye_gauss_f, img_f, out=np.zeros_like(img_f), where=img_f != 0
+                )
+                f3 = np.divide(
+                    nose_gauss_f, img_f, out=np.zeros_like(img_f), where=img_f != 0
+                )
+                f4 = np.divide(
+                    mouth_gauss_f, img_f, out=np.zeros_like(img_f), where=img_f != 0
                 )
                 self.left_eye_asef += f1
                 self.right_eye_asef += f2
+                self.nose_asef += f3
+                self.mouth_asef += f4
                 taken += 1
             except:
                 skipped += 1
                 continue
+        # were you trying to normalize the fft? @Abdulhady
         self.left_eye_asef / taken
         self.right_eye_asef / taken
+        self.nose_asef / taken
+        self.mouth_asef / taken
         print(f'Finished training, taken = {taken}, skipped = {skipped}')
 
     def gauss(self, x, y, sigma=4.0):
@@ -89,10 +116,18 @@ class eyelocator:
         # img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
         img = cv2.resize(image, [128, 128])
         img_f = fft.fft2(img)
+        # detection
         left_eye_img = fft.ifft2(img_f * self.left_eye_asef)
         right_eye_img = fft.ifft2(img_f * self.right_eye_asef)
+        nose_img = fft.ifft2(img_f * self.nose_asef)
+        mouth_img = fft.ifft2(img_f * self.mouth_asef)
         x1, y1 = np.unravel_index(left_eye_img.argmax(), left_eye_img.shape)
         x2, y2 = np.unravel_index(right_eye_img.argmax(), right_eye_img.shape)
+        x3, y3 = np.unravel_index(nose_img.argmax(), nose_img.shape)
+        x4, y4 = np.unravel_index(
+            mouth_img.argmax(), mouth_img.shape)
         cv2.ellipse(img, (x1, y1), (9, 4), 0, 0, 360, color=255, thickness=1)
         cv2.ellipse(img, (x2, y2), (9, 4), 0, 0, 360, color=255, thickness=1)
+        cv2.ellipse(img, (x3, y3), (9, 4), 0, 0, 360, color=255, thickness=1)
+        cv2.ellipse(img, (x4, y4), (9, 4), 0, 0, 360, color=255, thickness=1)
         return img
--- EyeLocator/train.py
+++ EyeLocator/train.py
@@ -5,7 +5,7 @@ from lazyarray import LazyArray
 
 if __name__ == "__main__":
     locator = eyelocator()
-    data_size = 1491
+    data_size = 1700
     lazy_images = LazyArray(
         lambda i: cv2.imread(f'./face-training-set/ ({i+1}).jpg'), data_size
     )
@@ -15,4 +15,6 @@ if __name__ == "__main__":
     print("\nSaving filters...")
     np.save('results/left_eye_asef', locator.left_eye_asef)
     np.save('results/right_eye_asef', locator.right_eye_asef)
+    np.save('results/mouth_asef', locator.mouth_asef)
+    np.save('results/nose_asef', locator.nose_asef)
     print("filters saved!")
