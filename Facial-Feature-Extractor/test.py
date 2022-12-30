9d6323a2d69f9430e545ac71530d159814c2ed97
Mohammed Abdulhady <m.abdulhady00@gmail.com>
2022-12-30 03:42:14 +0200

    fix: distance metric

--- Facial-Feature-Extractor/test.py
+++ Facial-Feature-Extractor/test.py
@@ -1,16 +1,12 @@
 import cv2
 import dlib
-
-# import imutils
-import numpy as np
-import scipy.fft as fft
-from progress.bar import Bar
 from ffextractor import FeaturesLocator
+from progress.bar import Bar
 
 
 def distance(x1, y1, x2, y2):
-    # distance smaller thn 10 pixels
-    return ((x1-x2)**2 + (y1-y2)**2)**1/2 < 30
+    # distance smaller than 20 pixels
+    return ((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2)) < (20 * 20)
 
 
 if __name__ == "__main__":
@@ -25,19 +21,19 @@ if __name__ == "__main__":
     nose_false = 0
     mouth_false = 0
     detector = dlib.get_frontal_face_detector()
-    predictor = dlib.shape_predictor(
-        "./shape_predictor_68_face_landmarks.dat")
+    predictor = dlib.shape_predictor("./shape_predictor_68_face_landmarks.dat")
     face_cascade = cv2.CascadeClassifier(
         cv2.data.haarcascades + 'haarcascade_frontalface_default.xml'
     )
     print("\nStarted testing...")
-    for i in range(data_size):
+    bar = Bar()
+    for i in bar.iter(range(data_size)):
         try:
-            img = cv2.imread(f'./face-training-set/ ({i+100}).jpg')
+            img = cv2.imread(f'./face-testing-set/{i+100}.jpg')
             img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
             faces = face_cascade.detectMultiScale(img, 1.3, 5)
             (y, x, h, w) = faces[0]
-            img = cv2.resize(img[x: x + w, y: y + h], (128, 128))
+            img = cv2.resize(img[x : x + w, y : y + h], (128, 128))
             features = locator.findfeatures(img)
             detect = detector(img, 1)
             shape = predictor(img, detect[0])
@@ -74,17 +70,15 @@ if __name__ == "__main__":
         except:
             continue
     print(
-        f"Left Eye accuracy: {left_eye_correct*100.0/(left_eye_correct+left_eye_false)}%")
+        f"Left Eye accuracy: {left_eye_correct*100.0/(left_eye_correct+left_eye_false)}%"
+    )
     print(f"correct: {left_eye_correct}, false: {left_eye_false}")
     print(
-        f"Right Eye accuracy: {right_eye_correct*100.0/(right_eye_correct+right_eye_false)}%")
+        f"Right Eye accuracy: {right_eye_correct*100.0/(right_eye_correct+right_eye_false)}%"
+    )
     print(f"correct: {right_eye_correct}, false: {right_eye_false}")
-    print(
-        f"Nose accuracy: {nose_correct*100.0/(nose_correct+nose_false)}%")
-    print(
-        f"correct: {nose_correct}, false: {nose_false}")
-    print(
-        f"Mouth accuracy: {mouth_correct*100.0/(mouth_correct+mouth_false)}%")
+    print(f"Nose accuracy: {nose_correct*100.0/(nose_correct+nose_false)}%")
+    print(f"correct: {nose_correct}, false: {nose_false}")
+    print(f"Mouth accuracy: {mouth_correct*100.0/(mouth_correct+mouth_false)}%")
 
-    print(
-        f"correct: {mouth_correct}, false: {mouth_false}")
+    print(f"correct: {mouth_correct}, false: {mouth_false}")
